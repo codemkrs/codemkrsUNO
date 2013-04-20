117699a3a3034bcc2dd04592bd3fcf6014d1077a
Scott Hedrick <scotthedrick0@gmail.com>
2013-04-20 00:52:51 -0500

    pushing server.js

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(8124, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:8124/');
+}).listen(8124, "0.0.0.0");
+console.log('Server running at http://0.0.0.0:8124/');
