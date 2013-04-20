39d6290834f271c4ae9fb3bfd2b5b2dea835b925
Justen Martin <jmartin@arrowshooterstech.com>
2013-04-20 01:14:53 -0500

    added twitter bootstrap

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8124, "0.0.0.0");
-console.log('Server running at http://0.0.0.0:8124/');
+(function(){
+	var express = require('express');
+	var app = express();
+
+	app.listen(3000);
+	console.log('Server listening on 3000')
+})();
